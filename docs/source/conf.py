47a9e3f856fa7b7acb0e987b92201f223beefee8
Dmitry Pershin <dapper1291@gmail.com>
2019-11-09 17:20:08 +0500

    some minor fixes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,6 +66,8 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# The master toctree document.
+master_doc = 'index'
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
