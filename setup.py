3bac885bf4f6d31ce2d35542a3a1074971fb3fb3
Dmitry Pershin <dapper1291@gmail.com>
2021-03-01 23:51:16 +0500

    python3.9 support added.

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     project_urls={
         "Documentation": "https://pjrpc.readthedocs.io/en/latest/",
