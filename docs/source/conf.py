9bbe93c4bcd816ebfba059a343773d1b264b777c
Dmitry Pershin <dapper1291@gmail.com>
2021-08-10 23:38:15 +0500

    documentation custom style added.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,6 +66,8 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = ['css/custom.css']
+
 # The master toctree document.
 master_doc = 'index'
 
