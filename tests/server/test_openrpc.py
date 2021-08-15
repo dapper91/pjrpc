2b0a1cc5ab39bd7e1c899c52e446331c5e99c353
Dmitry Pershin <dapper1291@gmail.com>
2021-08-15 11:07:38 +0500

    open rpc specification generator tests fixed.

--- tests/server/test_openrpc.py
+++ tests/server/test_openrpc.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional
 
 import pydantic
 
@@ -18,7 +19,7 @@ def test_openapi_schema_generator(resources):
             contact=specs.Contact(name='owner', email='test@gmal.com'),
             license=specs.License(name='MIT'),
         ),
-        external_docs=specs.ExternalDocumentation(url='external-doc.com'),
+        external_docs=specs.ExternalDocumentation(url='http://external-doc.com'),
         servers=[
             specs.Server(
                 url='http://test-server',
@@ -36,15 +37,16 @@ def test_openapi_schema_generator(resources):
 
     class TestError(pjrpc.common.exceptions.JsonRpcError):
         code = 2001
-        message = 'test error'
+        message = 'test error 1'
 
     @specs.annotate(
         errors=[
             TestError,
-            specs.Error(code=2002, message='test error'),
+            specs.Error(code=2002, message='test error 2'),
         ],
         examples=[
             specs.MethodExample(
+                name='Simple example',
                 params=[
                     specs.ExampleObject(
                         name='param1',
@@ -78,44 +80,57 @@ def test_openapi_schema_generator(resources):
         Description
 
         :param ctx: context
-        :param int param1: param1
-        :param dict param2: param2
-        :return int: result
-        :raises: TestError
+        :param integer param1: param1 summary.
+                               description.
+        :param object param2: param2 summary.
+                              description.
+        :return integer: result summary.
+                         description.
+        :raises TestError: test error
         """
 
-    def method2(param1: int, param2) -> Model:
+    def method2(param1: int = 1, param2: Optional[float] = None) -> Optional[str]:
         """
         Method2.
 
-        :param param1: param1
-        :type param1: int
-        :param param2: param2
-        :type param2: dict
-        :return dict: result
+        Description.
+
+        :param integer? param1: param1 summary.
+        :param number? param2: param2 summary.
+        :return string: result summary.
         """
 
     @specs.annotate(
         params_schema=[
             specs.ContentDescriptor(
                 name='param1',
-                schema={},
+                schema={'type': 'number'},
                 summary='param1 summary',
                 description='param1 description',
             ),
         ],
         result_schema=specs.ContentDescriptor(
             name='result',
-            schema={},
+            schema={'type': 'number'},
+            summary='result summary',
+            description='result description',
         ),
     )
-    def method3(*args, **kwargs):
+    def method3(param1: int) -> int:
+        pass
+
+    def method4(*args, **kwargs) -> None:
+        pass
+
+    def method5():
         pass
 
     method1 = Method(method1, 'method1', 'ctx')
     method2 = Method(method2, 'method2')
     method3 = Method(method3, 'method3')
+    method4 = Method(method4, 'method4')
+    method5 = Method(method5, 'method5')
 
-    actual_schema = spec.schema('/test/path', methods=[method1, method2, method3])
+    actual_schema = spec.schema('/test/path', methods=[method1, method2, method3, method4, method5])
 
     assert actual_schema == resources('openrpc-1.json', loader=json.loads)
