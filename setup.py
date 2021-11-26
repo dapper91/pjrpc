72734b2dc0bcf0eaa334664a14e30cf8dedc2075
Dmitry Pershin <dapper1291@gmail.com>
2021-11-27 00:31:33 +0500

    python 3.10 support added.

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     project_urls={
         "Documentation": "https://pjrpc.readthedocs.io/en/latest/",
