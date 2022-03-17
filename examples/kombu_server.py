a16fada70222ff856231957dd46c49c6709855c1
Bernhard Kaindl <bernhard.kaindl@autonoma.cloud>
2022-03-17 13:58:56 +0100

    examples/kombu_*.py: Add methods and use jsonrpc a non-durable queue
    Implement the sum(a, b) and tick RPC endpoints for kombu_client.py
    and aio_pika_client.py and use queue_args={"durable": False} to be
    able to connect to the "jsonrpc" queue created by
    examples/aio_pika_*.py. Makes it also interoperable with these examples.
    
    examples/kombu_server.py and examples/kombu_client.py appear to have an
    issue communicating with each other: The RPC gets stuck very often, but
    server/client cross-operation with examples/aio_pika_*.py works fine.
    This issue is present with the "jsonrpc" set to durable as well.

--- examples/kombu_server.py
+++ examples/kombu_server.py
@@ -15,8 +15,28 @@ def add_user(message: kombu.Message, user: dict):
     return {'id': user_id, **user}
 
 
-executor = integration.Executor('amqp://guest:guest@localhost:5672/v1', queue_name='jsonrpc')
-executor.dispatcher.add_methods(methods)
+@methods.add  # type: ignore
+def sum(a: int, b: int) -> int:
+    """RPC method sum(a, b) for kombu_client.py and aio_pika_client.py"""
+    return a + b
+
+
+@methods.add  # type: ignore
+def tick() -> None:
+    """RPC notification "tick" for kombu_client.py and aio_pika_client.py"""
+    print("received tick")
+
+
+# Note: The server may not work well with examples/kombu_client.py yet.
+# Use with examples/aio_pika_client.py in case server or client gets stuck.
 
 if __name__ == "__main__":
+    executor = integration.Executor(
+        "amqp://guest:guest@localhost:5672/v1",
+        queue_name="jsonrpc",
+        # Compatible with queue of examples/aio_pika_*
+        # and works better with examples/kombu_client.py:
+        queue_args={"durable": False},
+    )
+    executor.dispatcher.add_methods(methods)
     executor.run()
